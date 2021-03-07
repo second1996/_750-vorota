8bdcc44291cda91a9df175b37c5e395e48d12144
second1996 <vital10012@gmail.com>
2021-03-07 03:12:12 +0200

    Header (part 1 of 3)

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,15 @@
-// // Import jQuery module (npm i jquery)
-// import $ from 'jquery'
-// window.jQuery = $
-// window.$ = $
+// Import jQuery module
+import $ from 'jquery'
+window.jQuery = $
+window.$ = $
 
-// // Import vendor jQuery plugin example (not module)
-// require('~/app/libs/mmenu/dist/mmenu.js')
+// Import vendor jQuery plugin example (not module)
+require('../../node_modules/popper.js/dist/popper.min.js') // Popper lib
+require('../../node_modules/bootstrap/js/dist/util.js') // Bootstrap Util lib
+require('../../node_modules/bootstrap/js/dist/dropdown.js') // Bootstrap Dropdown
+
+// Another imports
+require('./_scripts.js')
 
 document.addEventListener('DOMContentLoaded', () => {
 	// JS code...
