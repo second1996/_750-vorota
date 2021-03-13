3460f17f29b97db7d92e46ab53394fe9c5431512
second1996 <vital10012@gmail.com>
2021-03-13 19:11:29 +0200

    Responsive mobile. Code refactoring.

--- app/js/_sliders.js
+++ app/js/_sliders.js
@@ -6,9 +6,9 @@ SwiperCore.use([Autoplay, Navigation, Pagination])
 // Home page: Heroes main slider
 new Swiper('.h-heroes-slider', {
 	loop: true,
-	// autoplay: {
-	// 	delay: 5000,
-	// },
+	autoplay: {
+		delay: 3500,
+	},
 	navigation: {
 		prevEl: '.h-heroes-slider-nav .swiper-button-prev',
 		nextEl: '.h-heroes-slider-nav .swiper-button-next',
@@ -35,8 +35,8 @@ $('.products-slider').each(function (index, element) {
 	$(element).addClass(`products-slider-${id}`)
 
 	new Swiper(`.products-slider-${id} .swiper-container`, {
-		slidesPerView: 6,
-		spaceBetween: 24,
+		slidesPerView: 2,
+		spaceBetween: 16,
 		navigation: {
 			prevEl: `.products-slider-${id} .products-slider-nav .swiper-button-prev`,
 			nextEl: `.products-slider-${id} .products-slider-nav .swiper-button-next`,
@@ -45,13 +45,28 @@ $('.products-slider').each(function (index, element) {
 			el: `.products-slider-${id} .products-slider-dots .swiper-pagination`,
 			type: 'bullets',
 		},
+		breakpoints: {
+			576: {
+				slidesPerView: 3,
+				spaceBetween: 24,
+			},
+			768: {
+				slidesPerView: 4,
+			},
+			992: {
+				slidesPerView: 5,
+			},
+			1240: {
+				slidesPerView: 6,
+			}
+		}
 	})
 })
 
 // Home page: Partners slider
 new Swiper('.h-partners-slider .swiper-container', {
-	slidesPerView: 9,
-	spaceBetween: 24,
+	slidesPerView: 3,
+	spaceBetween: 16,
 	loop: true,
 	navigation: {
 		prevEl: '.h-partners-slider-nav .swiper-button-prev',
@@ -61,12 +76,31 @@ new Swiper('.h-partners-slider .swiper-container', {
 		el: '.h-partners-slider-dots .swiper-pagination',
 		type: 'bullets',
 	},
+	breakpoints: {
+		576: {
+			slidesPerView: 4,
+			spaceBetween: 24,
+		},
+		768: {
+			slidesPerView: 5,
+		},
+		992: {
+			slidesPerView: 6,
+		},
+		1240: {
+			slidesPerView: 7,
+		},
+		1468: {
+			slidesPerView: 9,
+		}
+	}
 })
 
 // Home page: Testimonials slider
 new Swiper('.h-testimonials-slider .swiper-container', {
-	slidesPerView: 2,
-	spaceBetween: 24,
+	slidesPerView: 1,
+	spaceBetween: 16,
+	autoHeight: true,
 	navigation: {
 		prevEl: '.h-testimonials-slider-nav .swiper-button-prev',
 		nextEl: '.h-testimonials-slider-nav .swiper-button-next',
@@ -75,4 +109,11 @@ new Swiper('.h-testimonials-slider .swiper-container', {
 		el: '.h-testimonials-slider-dots .swiper-pagination',
 		type: 'bullets',
 	},
+	breakpoints: {
+		992: {
+			slidesPerView: 2,
+			spaceBetween: 24,
+			autoHeight: false,
+		}
+	}
 })
--- app/js/app.js
+++ app/js/app.js
@@ -10,22 +10,28 @@ require('../../node_modules/bootstrap/js/dist/dropdown.js') // Bootstrap Dropdow
 require('../../node_modules/bootstrap/js/dist/tab.js') // Bootstrap Tab
 require('../../node_modules/jquery-mask-plugin/dist/jquery.mask.min.js') // jQuery Mask
 
-$(document).on('ready', function() {
+document.addEventListener('DOMContentLoaded', () => {
 	// Another imports
 	require('./_scripts.js')
 	require('./_sliders.js')
 
-	$('#header-search-field').on('click', function() {
-		const searchEl = $('.header-middle-search')
-
+	// Toggle Search results
+	const searchEl = $('.header-middle-search')
+	$('#header-search-field, .header-middle-actions .search').on('click', function() {
 		searchEl.addClass('_is-focused')
 		searchEl.find('.search-results').addClass('_is-opened')
 
 		$(document).on('click', function(e) {
-			if (!searchEl.is(e.target) && searchEl.has(e.target).length === 0) {
+			if ((!searchEl.is(e.target) && !$('.header-middle-actions .search').is(e.target)) && (searchEl.has(e.target).length === 0 && $('.header-middle-actions .search').has(e.target).length === 0)) {
 				searchEl.removeClass('_is-focused')
 				searchEl.find('.search-results').removeClass('_is-opened')
 			}
 		})
 	})
+	
+	$('.header-middle-search .heading .btn-close').on('click', function() {
+		searchEl.removeClass('_is-focused')
+		searchEl.find('.search-results').removeClass('_is-opened')
+	})
+
 })
